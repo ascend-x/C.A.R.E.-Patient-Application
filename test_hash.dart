03c68d0814267e90eb1f2510252ff1e63ebcc9a0
Nandakishore V <kishore181206@gmail.com>
2026-02-22 02:51:26 +0530

    chore: Delete temporary hash testing script.

--- test_hash.dart
+++
@@ -1,9 +0,0 @@
-import 'package:web3dart/crypto.dart';
-import 'package:convert/convert.dart';
-import 'dart:convert';
-
-void main() {
-  final sig = 'getRecords(address)';
-  final hash = hex.encode(keccak256(utf8.encode(sig)));
-  print('Function selector: 0x\${hash.substring(0, 8)}');
-}
